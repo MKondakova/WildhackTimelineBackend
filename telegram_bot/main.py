2ceda6c73a94de54af7d07fa388f1e7255ef300d
MKondakova <milanon@ya.ru>
2021-12-04 13:35:51 +0300

    Гитигнор

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -22,30 +22,30 @@ def handle_post(message):
 	if (len(title) <= 1):
 		msg = bot.reply_to(message, "А заголовок где?")
 		return
-	print(title[1:].join(' '))
-	fact={ 'title': title[1:].join(' ') }
+	print(' '.join(title[1:]))
+	fact={ 'title': ' '.join(title[1:]) }
 	msg = bot.reply_to(message, 'Жду дату в ответ на это сообщение')
 	bot.register_next_step_handler(msg, handle_post_data, fact=fact)
 
 def handle_post_data(message, fact):
 	date = message.text.split()
-	if (len(date) <= 1):
+	if (len(date) == 0):
 		msg = bot.reply_to(message, "А дата где?")
 		bot.register_next_step_handler(msg, handle_post_data, fact=fact)
 		return 
-	print(date[1])
-	fact['date'] = date[1]
+	print(date[0])
+	fact['date'] = date[0]
 	msg = bot.reply_to(message, 'Жду текст в ответ на это сообщение')
 	bot.register_next_step_handler(msg, handle_post_text, fact=fact)
 
 def handle_post_text(message, fact):
-	text = message.text.split()
-	if (len(text) <= 1):
+	text = message.text
+	if (len(text) == 0):
 		msg = bot.reply_to(message, "А текст где?")
 		bot.register_next_step_handler(msg, handle_post_text, fact=fact)
 		return 
-	print(text[1:].join(' '))
-	fact['text'] = text[1:].join(' ')
+	print(text)
+	fact['text'] = text
 	msg = bot.reply_to(message, 'Молодец, так держать!')
 	send_fact(fact)
 
